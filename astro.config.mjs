ffc6f27c21699dc936d9d344b1d20b5181412eba
Edward Grant <edward.grant@vmlyr.com>
2026-05-06 12:38:45 -0400

    Deploy Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://masternone.github.io',
+	base: '/animation-playground',
+});
